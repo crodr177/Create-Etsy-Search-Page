ed0b0073661e5c0e10ec7a14773ef3f3dbcb8848
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2019-03-25 21:58:41 -0700

    updated files for better look

---
+++ main1.js
@@ -0,0 +1,34 @@
+var itemContainer = document.querySelector("#item-container");
+
+var htmlstr = items.results.map(function(item) {
+
+   var name;
+    if(item.title.length > 30) {
+        name = item.title.substr(0, 30) + "...";
+    }
+    else {
+        name = item.title;
+    }
+
+   var image = item.Images[0].url_fullxfull;
+   var maker = item.Shop.shop_name;
+   var price = item.price;
+   var rating = item.views;
+
+  return `
+    <div class ="data">
+        <a href="${item.url}"><img src="${image}" class="image"></a>
+        <p class="name"><abbr title="${item.title}">${name}</abbr></p>
+        <p class="maker">${maker}</p>
+        <div class="rating-section">
+          <p class="rating">&#9733; &#9733; &#9733; &#9733; &#9733;</p>
+          <p class="rating-num">(${rating})</p>
+        </div>
+        <p class="price">$${price}</p>
+        <i class="far fa-heart fa-lg"></i>
+    </div>
+    `
+
+  }).join("");
+
+  itemContainer.innerHTML = htmlstr;
